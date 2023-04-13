2e4e24b19e1610e3e128016f312dd04961e966b6
KseniyaGulina <75985144+KseniyaGulina@users.noreply.github.com>
2023-04-13 17:11:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,8 +11,9 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    alpha = 0.02
     count = np.array([x_success, y_success])
     nobs = np.array([x_cnt, y_cnt])
     stat, pval = proportions_ztest(count, nobs, alternative='smaller')
-    result = pval < 0.02
+    result = pval < alpha
     return result
